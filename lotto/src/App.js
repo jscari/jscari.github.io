a0d919aecdaea53cf3abed9bdcf24340d763c8b4
jscari <js.carinos@gmail.com>
2020-12-24 20:13:48 -0500

    fix lotto

--- lotto/src/App.js
+++ lotto/src/App.js
@@ -7,11 +7,16 @@ class App extends Component {
   constructor(props) {
     super();
     this.newBall = this.newBall.bind(this);
+    this.newBoard = this.newBoard.bind(this);
     const numbers = [];
     for (let i = 1; i < 100; i++) { numbers.push(i); }
     this.state = { balls: [], availableNumbers: numbers, pickedNumbers: [] };
   }
   render() {
+    const newBoard = this.state.balls.length === 0 ? 
+    (<button className="midnight-blue-flat-button" onClick={this.newBoard}>
+      Nueva tarjeta
+    </button>) : null;
     return (
       <div className="App">
         <div id="ballpark">
@@ -21,6 +26,7 @@ class App extends Component {
           <button className="midnight-blue-flat-button" onClick={this.newBall}>
             Nuevo Número
           </button>
+          { newBoard}
         </div>
       </div>
     );
@@ -37,6 +43,13 @@ class App extends Component {
     balls.sort(function(a, b){ return a.props.n[0] - b.props.n[0]});
     balls.unshift( <Ball n={n} key={n} isLast={true}/>);
     this.setState({ balls: balls, pickedNumbers: pickedNumbers, availableNumbers: availableNumbers, lastBall: n })
+  }
+  newBoard() {
+    for (let i=0; i < 16; i++) {
+      setTimeout(() => {this.newBall()}, 200)
+      ;
+    }
+    document.getElementsByClassName('lastBall')[0].style.display = 'none'
   }
 }
 
